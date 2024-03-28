0edeb0c5474485b8d47a500eaf1c6229cfa2df5a
Meorwik <104008026+Meorwik@users.noreply.github.com>
2024-03-28 07:56:04 +0500

    empty page website with title and icon created

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
+
 app = FastAPI(debug=True)
+app.mount("/assets", StaticFiles(directory="src/assets"))
+templates = Jinja2Templates(directory="src/templates")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.jinja2"
+    )
 
 
 @app.get("/hello/{name}")
